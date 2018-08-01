772aa3c5b070f7a88c80e2331b1b257bf787a0c6
zhuxudong <callzhuxudong@163.com>
2018-08-01 20:40:39 +0800

    README.md

--- README.md
+++ README.md
@@ -1,3 +1,79 @@
 # babylonWander
-> 基于Babylon的FreeCamera的漫游插件
+> 基于Babylon的漫游手柄插件,该插件使用自己的配置来实现漫游功能，具体配置参考下面option
 
+## Global
+```
+<script src="babylonWander.min.js"></script>  
+<script>
+    babylonWander(camera)
+</script>
+```
+## NPM
+npm install
+```
+npm install babylonwander --save
+```
+use with ES6
+```
+import babylonWander from 'babylonWander'
+babylonWander(camera)
+```
+or you can use with commonJS
+```
+let babylonWander=require('babylonWander')
+babylonWander(camera)
+```
+
+## option
+```
+let wander=babylonWander(camera,option)
+```
+* camera:创建的漫游相机
+* option:漫游相关配置,默认配置如下
+   > 默认canvas的父对象,可以通过 wander.setWrapper来更改
+   * wrapper: canvas.parentElement
+   > 碰撞椭圆体
+   * ellipsoid: new BABYLON.Vector3(.5, 2, .5)
+   > 重力
+   * gravity: new BABYLON.Vector3(0, -9.8, 0)
+   > 漫游code码,默认←↑→↓和AWDS
+   * keysLeft: [37, 65]
+   * keysUp: [38, 87]
+   * keysRight: [39, 68]
+   * keysDown: [40, 83]
+   > 跳跃code码,默认space
+   * keysJump: [32]
+   > 漫游速度
+   * speed: 1
+   > 弹跳能力
+   * jumpingAbility: 30
+   > 允许弹跳间隔时间
+   * jumpingTime: 600
+   > 漫游灵敏度,50代表50像素达到最大值，所以数值越大越不灵敏
+   * wanderSensibility: 50
+   > 旋转视角灵敏度
+   * rotateSensibility: 100
+
+
+## API
+```
+let wander=babylonWander(camera,option)
+```
+> 重新设置包裹漫游插件的DOM
+* wander.setWrapper(dom) 
+> 获取漫游插件的配置项
+* wander.getOption() 
+> 设置漫游插件的配置项
+* wander.setOption({...}) 
+> 显示插件,并开启事件
+* wander.show()
+> 隐藏插件,disposeEvent决定是否隐藏的同时也销毁事件
+* wander.hide(disposeEvent?: boolean) 
+> 手动销毁漫游插件.当babylon引擎销毁的时候会自动销毁漫游插件
+* wander.dispose() 
+
+
+![demo](//http://zhuxudong.cn/github/img/babylonWander/1.jpg)
+
+
+ 
--- src/demo/demo.ts
+++ src/demo/demo.ts
@@ -1,56 +1,57 @@
 import * as BABYLON from "babylonjs"
 
 let wander = require("../app.ts").default
-declare let window: any
 
 class Demo {
-  canvas: any
-  engine: any
-  scene: any
-  camera: any
+    canvas: any
+    engine: any
+    scene: any
+    camera: any
 
-  constructor() {
-    this.canvas = document.getElementById("renderCanvas");
-    this.engine = new BABYLON.Engine(this.canvas, true, {
-      preserveDrawingBuffer: true,
-      stencil: true,
-      doNotHandleContextLost: true
-    });
-    this.scene = new BABYLON.Scene(this.engine);
-    this.scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
-    //FreeCamera
-    this.camera = new BABYLON.FreeCamera("freeCamera", new BABYLON.Vector3(0, 0, 0), this.scene)
-    this.camera.attachControl(this.canvas, true);
-    let light0 = new BABYLON.HemisphericLight("hem1", new BABYLON.Vector3(0, 1, 0), this.scene);
-    light0.intensity = .5;
-    this.engine.runRenderLoop(() => {
-      this.scene.render();
-    })
-    window.addEventListener("resize", () => {
-      this.engine.resize();
-    });
+    constructor() {
+        this.canvas = document.getElementById("renderCanvas");
+        this.engine = new BABYLON.Engine(this.canvas, true, {
+            preserveDrawingBuffer: true,
+            stencil: true,
+            doNotHandleContextLost: true
+        });
+        this.scene = new BABYLON.Scene(this.engine);
+        this.scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+        //FreeCamera
+        this.camera = new BABYLON.FreeCamera("freeCamera", new BABYLON.Vector3(0, 0, 0), this.scene)
+        this.camera.attachControl(this.canvas, true);
+        let light0 = new BABYLON.HemisphericLight("hem1", new BABYLON.Vector3(0, 1, 0), this.scene);
+        light0.intensity = .5;
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        })
+        window.addEventListener("resize", () => {
+            this.engine.resize();
+        });
 
-    function random(a: any, b: any) {
-      return Math.random() * (b - a) + a
+        function random(a: any, b: any) {
+            return Math.random() * (b - a) + a
+        }
+
+        let ball = BABYLON.Mesh.CreateSphere("ball", 50, 32, this.scene);
+        for (let i = 0; i < 100; i++) {
+            let instance = ball.createInstance("ball" + i);
+            instance.position = new BABYLON.Vector3(random(-2000, 2000), random(-25, 100), random(-1000, 1000))
+        }
+        let ground = BABYLON.Mesh.CreateGround("ground", 4000, 4000, 1, this.scene)
+        let grdMat = new BABYLON.StandardMaterial("grdMat", this.scene)
+        let marbleUrl = require("./marble.jpg")
+        let grdTex = new BABYLON.Texture(marbleUrl, this.scene)
+        grdTex.uScale = grdTex.vScale = 100;
+        grdMat.diffuseTexture = grdTex;
+        ground.material = grdMat;
+        ground.position.y = -50
+        ground.checkCollisions = true;
     }
-
-    let ball = BABYLON.Mesh.CreateSphere("ball", 50, 32, this.scene);
-    for (let i = 0; i < 100; i++) {
-      let instance = ball.createInstance("ball" + i);
-      instance.position = new BABYLON.Vector3(random(-2000, 2000), random(-25, 100), random(-1000, 1000))
-    }
-    let ground = BABYLON.Mesh.CreateGround("ground", 4000, 4000, 1, this.scene)
-    let grdMat = new BABYLON.StandardMaterial("grdMat", this.scene)
-    let marbleUrl = require("./marble.jpg")
-    let grdTex = new BABYLON.Texture(marbleUrl, this.scene)
-    grdTex.uScale = grdTex.vScale = 100;
-    grdMat.diffuseTexture = grdTex;
-    ground.material = grdMat;
-    ground.position.y = -50
-    ground.checkCollisions = true;
-  }
 }
 
 let demo = new Demo()
-window.demo = demo;
-window.test = wander(demo.camera, {})
+let w = wander(demo.camera, {
+    speed: .5
+})
+console.log(w)
